92a4c6f4e2e8cb176f324fc8195fb2864e297e34
adamwilkinson3 <82132359+adamwilkinson3@users.noreply.github.com>
2022-05-23 00:40:36 -0400

    changed /home to /

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/home' element={<Home />} />
+        <Route path='/' element={<Home />} />
         <Route path='/login' element={<Register />} />
       </Routes>
     </div>
